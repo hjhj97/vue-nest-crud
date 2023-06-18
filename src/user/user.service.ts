3a24b8ce0c1f2f419aa3dc2aef6e3bff4c85074a
JuHeonHa <hjhjaueon97@naver.com>
2023-06-18 14:34:49 +0900

    name 유일성 검증

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -14,7 +14,11 @@ export class UserService {
   async createUser(createUserDto: CreateUserDto) {
     const { name, password } = createUserDto;
     const newUser = this.userRepository.create({ name, password });
-    await this.userRepository.save(newUser);
+    try {
+      await this.userRepository.save(newUser);
+    } catch (error) {
+      throw new ConflictException('이미 존재하는 name 입니다');
+    }
     return { success: true };
   }
 
