de837c3971c46ae3a8af2424d03ef4d6cdca5c8e
JuHeonHa <hjhjaueon97@naver.com>
2023-06-19 02:19:38 +0900

    frontend 디렉토리 생성

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>Home</div>
+</template>
+
+<script>
+	import axios from '@/plugins/axiosSetup';
+	import { onMounted } from 'vue';
+	export default {
+		setup() {
+			onMounted(async () => {
+				const res = await axios.get('/');
+				console.log(res);
+			});
+			return {};
+		},
+	};
+</script>
+
+<style lang="scss" scoped></style>
