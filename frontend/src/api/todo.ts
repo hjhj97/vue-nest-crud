9f39d79a4db3d1b5fe7bdec6b9a63aa8990d58be
JuHeonHa <hjhjaueon97@naver.com>
2023-06-19 16:49:38 +0900

    FE : todo 추가 기능 개발

--- frontend/src/api/todo.ts
+++ frontend/src/api/todo.ts
@@ -3,3 +3,6 @@ import axios from '@/plugins/axiosSetup';
 export const getMyTodos = async () => {
 	return await axios.get('/todo');
 };
+export const createTodo = async (title: string) => {
+	return await axios.post('/todo', { title });
+};
--- frontend/src/views/TodoView.vue
+++ frontend/src/views/TodoView.vue
@@ -1,19 +1,27 @@
 <template>
 	<div>
 		<h1>Todo</h1>
+		<form @submit="handleSubmit">
+			<input type="text" v-model="todoTitle" />
+			<button type="submit">추가</button>
+		</form>
 		<ul>
-			<li v-for="todo in todos" :key="todo.id">{{ todo.title }}</li>
+			<li v-for="todo in todos" :key="todo.id">
+				<input type="checkbox" :checked="todo.isCompleted" />
+				{{ todo.title }}
+			</li>
 		</ul>
 	</div>
 </template>
 
 <script lang="ts">
 	import { defineComponent, onMounted, ref } from 'vue';
-	import { getMyTodos } from '@/api/todo';
+	import { getMyTodos, createTodo } from '@/api/todo';
 
 	export default defineComponent({
 		setup() {
 			const todos = ref<any[]>([]);
+			const todoTitle = ref<string>('');
 			onMounted(() => {
 				fetchData();
 			});
@@ -24,8 +32,19 @@
 					todos.value = res;
 				}
 			};
+			const handleSubmit = async (e: Event) => {
+				e.preventDefault();
+				const res = await createTodo(todoTitle.value).catch((fail) => alert(fail));
+				if (res) {
+					fetchData();
+					todoTitle.value = '';
+				}
+			};
+
 			return {
 				todos,
+				todoTitle,
+				handleSubmit,
 			};
 		},
 	});
