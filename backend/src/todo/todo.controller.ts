c004f2ca6223a40ef3fd13c95034723e60a14781
JuHeonHa <hjhjaueon97@naver.com>
2023-06-19 16:02:26 +0900

    BE : 자신이 작성한 게시물 불러오기

--- backend/src/todo/todo.controller.ts
+++ backend/src/todo/todo.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { CreateTodoDto } from './dto/create-todo.dts';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { User } from 'src/user/user.entity';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt-auth.guard';
 
 @Controller('todo')
-export class TodoController {}
+@UseGuards(JwtAuthGuard)
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Get('/')
+  getMyTodos(@GetUser() user: User) {
+    return this.todoService.getMyTodos(user);
+  }
+
+  @Post('/')
+  @UsePipes(ValidationPipe)
+  createTodo(@Body() createTodoDto: CreateTodoDto, @GetUser() user: User) {
+    console.log(user);
+    return this.todoService.createTodo(createTodoDto, user);
+  }
+}
--- backend/src/todo/todo.service.ts
+++ backend/src/todo/todo.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Todo } from './todo.entity';
+import { Repository } from 'typeorm';
+import { CreateTodoDto } from './dto/create-todo.dts';
+import { User } from 'src/user/user.entity';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectRepository(Todo)
+    private readonly todoRepository: Repository<Todo>,
+  ) {}
+
+  async getMyTodos(user: User) {
+    const query = this.todoRepository
+      .createQueryBuilder('todo')
+      .where('todo.userId= :userId', { userId: user.id });
+    const todos = await query.getMany();
+    return todos;
+  }
+
+  async createTodo(createTodoDto: CreateTodoDto, user: User) {
+    const { title } = createTodoDto;
+
+    const newTodo = await this.todoRepository.save({
+      title,
+      isCompleted: false,
+      user,
+    });
+
+    await this.todoRepository.save(newTodo);
+    return newTodo;
+  }
+}
